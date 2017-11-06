388c976690b2d71d7bf0257f7b0eb84feaabd225
Aleksander Kuśmierczyk <Aleksander.Kusmierczyk@gmail.com>
2017-11-06 18:45:04 +0100

    adapted the program to be runable from command line

--- ACproject2/ACproject2/Program.cs
+++ ACproject2/ACproject2/Program.cs
@@ -11,20 +11,40 @@ namespace ACproject2
         static void Main(string[] args)
         {
             var tests = new Dictionary<string, int>();
-            //tests.Add("INPUT.csv", 100);
+
+            /*
+            tests.Add("INPUT.csv", 100);
             tests.Add("INPUT3.csv", 14);
             tests.Add("test1.csv", 3);
             tests.Add("test2.csv", 3);
             tests.Add("test3.csv", 3);
             tests.Add("test4.csv", 9);
-
+            
+            
             var loader = new InputLoader(';', @"..\..\..\Specification\input");
             var tester = new Tester(loader);
-
             foreach (var test in tests)
             {
                 tester.RunTest(test.Key, test.Value);
+            } 
+            */
+            //first argument is the path to the files second is the name of the files.
+            if (args.Length < 2)
+            {
+                Console.WriteLine("To use the application properly please write \"ACproject2\" directory_of_the_input input_file_name ");
             }
+            else if (args.Length == 2)
+            {
+                tests.Add(args[1], 3);
+                var loader = new InputLoader(',', args[0]);
+                var tester = new Tester(loader);
+                foreach (var test in tests)
+                {
+                    tester.RunTest(test.Key, test.Value);
+                }
+            }
+            else
+                Console.WriteLine("incorrect use of the program");
             Console.ReadLine();
         }
     }
